aae03788e00d7bad073b9836d29c35260c3abc74
J G <jordangladstone@gmail.com>
2019-02-17 14:58:50 -0500

    cleanup of example and IndexView stub

--- app/javascript/components/home_page_layout/HomePageLayout.js
+++ app/javascript/components/home_page_layout/HomePageLayout.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import NavMenu from '../nav_menu/NavMenu'
 import {
   Button,
   Container,
@@ -90,30 +91,9 @@ class DesktopContainer extends Component {
             style={{ minHeight: 700, padding: '1em 0em' }}
             vertical
           >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item as='a' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
+            <NavMenu 
+              fixed={fixed}
+            />
             <HomepageHeading />
           </Segment>
         </Visibility>
---
+++ app/javascript/components/views/IndexView.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
+import {
+    Container
+} from 'semantic-ui-react'
+
+class IndexView extends Component {
+
+    render() {
+
+        return (
+            <div>Hello!</div>
+        )
+    }
+}
+
+export default IndexView
